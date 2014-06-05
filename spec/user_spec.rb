2e269e51bc41b9d0e6fdf6340096f98b936659fb
Nossin A. Objio Saro <kosh61812@gmail.com>
2014-06-05 10:58:27 -0400

    implementado pruebas de user

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,11 +3,18 @@ require 'spec_helper'
 describe User do
 
   describe "#initialize" do
-    it "should properly initialize with an username"
+    it "should properly initialize with an username" do
+    user = User.new('Nossin')
+    expect(user).is_a?(User)
+    end
   end
 
   describe "#add_post" do
-    it "should add a post to the posts list"
+    it "should add a post to the posts list" do
+    user = User.new('Nossin')
+    user.add_post('Great times','ldkjslkjdslkdal','12/23/93')
+    expect(user.post_count).to eql(1)
+   end
   end
 
   describe "#list" do
@@ -15,7 +22,11 @@ describe User do
   end
 
   describe "#find_post_by_title" do
-    it "should return a post by title"
+    it "should return a post by title" do
+      user = User.new('Nossin')
+    user.add_post('Great times','ldkjslkjdslkdal','12/23/93')
+    expect(user.find_post_by_title).to eql('Great times')
+   end
     it "should return the first one if there are several with the same name"
   end
 
